9c51b3d65fd2c609719f0e89ced45bf6b4e8cfb9
mnehete32 <mnehete32@gmail.com>
2024-08-30 21:09:37 -0400

    updating main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import pickle
 from starter.ml.model import inference
 from starter.ml.data import process_data
 
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+# if "DYNO" in os.environ and os.path.isdir(".dvc"):
+#     os.system("dvc config core.no_scm true")
+#     if os.system("dvc pull") != 0:
+#         exit("dvc pull failed")
+#     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 app = FastAPI()
